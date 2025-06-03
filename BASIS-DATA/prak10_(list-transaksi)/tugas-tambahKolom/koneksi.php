a64c14f71992b0d8da04017fe17f344b7dd506ab
Itsnope <noverjkd@gmail.com>
2025-06-03 16:23:59 +0700

    basis-data-koneksi-errorHandling

--- BASIS-DATA/prak10_(list-transaksi)/tugas-tambahKolom/koneksi.php
+++ BASIS-DATA/prak10_(list-transaksi)/tugas-tambahKolom/koneksi.php
@@ -7,6 +7,8 @@ $db_name = 'prak10'; // Nama Database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {
 	die ('Gagal terhubung MySQL: ' . mysqli_connect_error());	
+} else {
+	echo "Berhasil terhubung ke MySQL";
 };
 
 $sql = 'SELECT id_produk, tgl_transaksi, harga, kuantitas FROM sales';
@@ -15,4 +17,6 @@ $query = mysqli_query($conn, $sql);
 
 if (!$query) {
 	die ('SQL Error: ' . mysqli_error($conn));
+} else {
+	echo "Query berhasil di eksekusi";
 };
